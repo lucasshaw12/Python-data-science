7ad359abc9b93e8b28b8619e9b652060938b14bd
Lucasshaw12 <shaw51@hotmail.co.uk>
2022-10-06 14:30:07 +0100

    Exercises for combining datasets (Pandas DataFrames & NumPy arrays).

--- combinedatasets.py
+++ combinedatasets.py
@@ -1,2 +1,271 @@
 #! python3
 # Combining datasets
+
+###########################
+# Combining Lists and Tuples with +
+###########################
+
+orders_2022_02_04 = [
+    (9423517, '2022-02-04', 9001),
+    (4626232, '2022-02-04', 9003),
+    (9423534, '2022-02-04', 9001)
+]
+
+orders_2022_02_05 = [
+    (9423679, '2022-02-05', 9002),
+    (4626377, '2022-02-05', 9003),
+    (9423534, '2022-02-05', 9003)
+]
+
+orders_2022_02_06 = [
+    (9423783, '2022-02-06', 9004),
+    (4626490, '2022-02-06', 9004)
+]
+
+orders = orders_2022_02_04 + orders_2022_02_05 + orders_2022_02_06
+# print(orders)
+
+
+###########################
+# Combining Dictionaries with **
+###########################
+
+extra_fields_9423517 = {
+    'ShippingInstructions': {
+        'name': 'john silver',
+        'Phone': [{'type': 'Office', 'number': '809-123-9309'},
+                  {'type': 'mobile', 'number': '417-123-4567'}
+                  ]
+    }
+}
+
+order_9423517 = {'OrderNo': 9423517, 'Date': '2022-02-04', 'Empno': 9001}
+order_9423517 = {**extra_fields_9423517, **order_9423517}
+# print(order_9423517)
+
+###########################
+# Combining corresponding rows 2 structures
+###########################
+
+details = [
+    (9423517, 'Jeans', 'Rip curl', 87.0, 1),
+    (9423517, 'Jacket', 'THe North Face', 112.0, 1),
+    (4626232, 'Socks', 'Vans', 15.0, 1),
+    (4626232, 'Jeans', 'Quiksilver', 82.0, 1),
+    (9423534, 'Socks', 'DC', 10.0, 2),
+    (9423534, 'Socks', 'Quiksilver', 12.0, 2),
+    (9423679, 'T-shirt', 'Patagonia', 35.0, 1),
+    (4626377, 'Hoody', 'Animal', 44.0, 1),
+    (4626377, 'Cargo Shorts', 'Animal', 38.0, 1),
+    (4626412, 'Shirt', 'Volcom', 78.0, 1),
+    (9423783, 'Boxer Shorts', 'Superdry', 30.0, 2),
+    (9423783, 'Shorts', 'Globe', 26.0, 1),
+    (4626490, 'Cargo Shorts', 'Billabong', 54.0, 1),
+    (4626490, 'Sweater', 'Dickies', 56.0, 1)
+]
+
+orders_details = []
+for o in orders:
+    for d in details:
+        if d[0] == o[0]:
+            orders_details.append(o + d[1:])
+# print(orders_details)
+
+###########################
+# Concatenating NumPy arrays
+###########################
+
+# Creating Numpy array
+import numpy as np
+
+jeff_salary = [2700, 3000, 3000]
+nick_salary = [2600, 2800, 2800]
+tom_salary = [2300, 2500, 2500]
+base_salary1 = np.array([jeff_salary, nick_salary, tom_salary])
+maya_salary = [2200, 2400, 2400]
+john_salary = [2500, 2700, 2700]
+base_salary2 = np.array([maya_salary, john_salary])
+base_salary = np.concatenate((base_salary1, base_salary2), axis=0)  # Combine salaries
+# print(base_salary)
+new_month_salary = np.array([[3000], [2900], [2500], [2500], [2700]])
+# print(new_month_salary)
+base_salary = np.concatenate((base_salary, new_month_salary), axis=1)
+# print(base_salary)
+
+###########################
+# Concatenating Pandas DataFrames
+###########################
+
+import pandas as pd
+
+salary_df1 = pd.DataFrame(
+    {'jeff': jeff_salary,
+     'nick': nick_salary,
+     'tom': tom_salary
+     }
+)
+salary_df1.index = ['June', 'July', 'August']  # Add this index to the DF
+salary_df1 = salary_df1.T
+# print(salary_df1)
+salary_df2 = pd.DataFrame(
+    {'maya': maya_salary,
+     'john': john_salary
+     },
+    index=['June', 'July', 'August']
+).T
+salary_df = pd.concat([salary_df1, salary_df2])  # Concatenate DF's
+# print(salary_df)
+salary_df3 = pd.DataFrame(
+    {'September': [3000, 2800, 2500, 2400, 2700],
+     'October': [3200, 3000, 2700, 2500, 2900]
+     },
+    index=['jeff', 'nick', 'tom', 'maya', 'john']
+)
+salary_df = pd.concat([salary_df, salary_df3], axis=1)  # Concatenate/add new columns
+# print(salary_df)
+salary_df = salary_df.drop(['September', 'October'], axis=1)  # Remove columns
+# print(salary_df)
+salary_df = salary_df.drop(['nick', 'maya'], axis=0)  # Remove rows
+# print(salary_df)
+
+###########################
+# Concatenating Pandas DataFrames with hierarchical Index
+###########################
+
+df_date_region1 = pd.DataFrame(
+    [
+        ('2022-02-04', 'East', 97.0),
+        ('2022-02-04', 'West', 243.0),
+        ('2022-02-05', 'East', 160.0),
+        ('2022-02-05', 'West', 35.0),
+        ('2022-02-06', 'East', 110.0),
+        ('2022-02-06', 'West', 860.0),
+    ],
+    columns=['Date', 'Region', 'Total']).set_index(['Date', 'Region'])
+
+df_date_region2 = pd.DataFrame(
+    [
+        ('2022-02-04', 'South', 114.0),
+        ('2022-02-05', 'South', 325.0),
+        ('2022-02-06', 'South', 212.0)
+    ],
+    columns=['Date', 'Region', 'Total']).set_index(['Date', 'Region'])
+
+df_date_region = pd.concat([df_date_region1, df_date_region2]).sort_index(level=['Date', 'Region'])  # Keep it sorted by date rather than
+# ...appending the 2nd DF to the end.
+# print(df_date_region)
+
+###########################
+# Concatenating Pandas DataFrames right-join
+###########################
+orders = [
+    (9423517, '2022-02-04', 9001),
+    (4626232, '2022-02-04', 9003),
+    (9423534, '2022-02-04', 9001),
+    (9423679, '2022-02-05', 9002),
+    (4626377, '2022-02-05', 9003),
+    (4626412, '2022-02-05', 9004),
+    (9423783, '2022-02-06', 9002),
+    (4626490, '2022-02-06', 9004)
+]
+
+details = [
+    (9423517, 'Jeans', 'Rip curl', 87.0, 1),
+    (9423517, 'Jacket', 'THe North Face', 112.0, 1),
+    (4626232, 'Socks', 'Vans', 15.0, 1),
+    (4626232, 'Jeans', 'Quiksilver', 82.0, 1),
+    (9423534, 'Socks', 'DC', 10.0, 2),
+    (9423534, 'Socks', 'Quiksilver', 12.0, 2),
+    (9423679, 'T-shirt', 'Patagonia', 35.0, 1),
+    (4626377, 'Hoody', 'Animal', 44.0, 1),
+    (4626377, 'Cargo Shorts', 'Animal', 38.0, 1),
+    (4626412, 'Shirt', 'Volcom', 78.0, 1),
+    (9423783, 'Boxer Shorts', 'Superdry', 30.0, 2),
+    (9423783, 'Shorts', 'Globe', 26.0, 1),
+    (4626490, 'Cargo Shorts', 'Billabong', 54.0, 1),
+    (4626490, 'Sweater', 'Dickies', 56.0, 1),
+]
+df_orders = pd.DataFrame(orders, columns=['OrderNo', 'Date', 'Empno'])
+df_details = pd.DataFrame(details, columns=['OrderNo', 'Item', 'Price', 'Brand', 'Quantity'])
+
+df_details = df_details.append(
+    {
+        'OrderNo': 4626592,
+        'Item': 'Shorts',
+        'Brand': 'Protest',
+        'Price': 48.0,
+        'Quantity': 1
+    },
+    ignore_index=True
+)
+df_orders_details_right = df_orders.merge(df_details, how='right', left_on='OrderNo', right_on='OrderNo')
+# print(df_orders_details_right)
+# print(df_orders_details_right.dtypes)  # Shows that 'EmpNo' have been converted to 'float64' from 'int64' due to 'NaN' value
+df_orders_details_right = df_orders_details_right.fillna({'Empno': 0}).astype({'Empno': 'int64'})  # Ensure 'EmpNo'...
+# ...returns to a 'int' value of '0' rather than a 'float
+# print(df_orders_details_right)
+# print(df_orders_details_right.dtypes)
+
+
+###########################
+# Concatenating Pandas DataFrames many-to-many join
+###########################
+
+books = pd.DataFrame(
+    {
+        'book_id': ['b1', 'b2', 'b3'],
+        'title': ['Beautifiul coding', 'Python for web development', 'Pythonic thinking'],
+        'topic': ['programming', 'Python, web', 'Python']
+    }
+)
+authors = pd.DataFrame(
+    {
+        'author_id': ['jsn', 'tri', 'wsn'],
+        'author': ['Johnson', 'Treloni', 'Wilson']
+    }
+)
+#  Create 3rd DF as associate to combine the 'author' and 'book' DF. Needs one-to-many with all other DF's
+matching = pd.DataFrame(
+    {
+        'author_id': ['jsn', 'jsn', 'tri', 'wsn'],
+        'book_id': ['b1', 'b2', 'b2', 'b3']
+    }
+)
+# print(matching)
+authorship = books.merge(matching).merge(authors)[['title', 'topic', 'author']]  # Merge 'books' with 'matching'...
+# ...then merge to 'authors'.
+print(authorship)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- dataframes.py
+++ dataframes.py
@@ -10,18 +10,18 @@ import numpy as np
 ##############
 
 # Creating Numpy array
-# jeff_salary = [2700, 3000, 3000]
-# nick_salary = [2600, 2800, 2800]
-# tom_salary = [2300, 250, 2500]
-# base_salary = np.array([jeff_salary, nick_salary, tom_salary])
+jeff_salary = [2700, 3000, 3000]
+nick_salary = [2600, 2800, 2800]
+tom_salary = [2300, 250, 2500]
+base_salary = np.array([jeff_salary, nick_salary, tom_salary])
 # print(base_salary)
-#
-# jeff_bonus = [500, 400, 400]
-# nick_bonus = [600, 300, 400]
-# tom_bonus = [200, 500, 400]
-# bonus = np.array([jeff_bonus, nick_bonus, tom_bonus])
-#
-# salary_bonus = base_salary + bonus
+
+jeff_bonus = [500, 400, 400]
+nick_bonus = [600, 300, 400]
+tom_bonus = [200, 500, 400]
+bonus = np.array([jeff_bonus, nick_bonus, tom_bonus])
+
+salary_bonus = base_salary + bonus
 # print(type(salary_bonus))
 # print(salary_bonus)
 # print(salary_bonus.max())  # Print maximum value within array
@@ -32,93 +32,95 @@ import numpy as np
 ##########################################
 # PANDAS
 ##########################################
+##############
 # Creating dataframe
-# data = ['Jeff', 'Jane', 'Tom']
-# emps_names = pd.Series(data)
+##############
+data = ['Jeff', 'Jane', 'Tom']
+emps_names = pd.Series(data)
 # print(emps_names)
 #
 ##############
-# # Accessing dataframe
+# Accessing dataframe
 ##############
-# emps_names = pd.Series(data, index=[9001, 9002, 9003])
+emps_names = pd.Series(data, index=[9001, 9002, 9003])
 # print(emps_names)
 # print(emps_names[9001])
 # print(emps_names.loc[9001])  # use index to find element
-# print(emps_names.iloc[0])  # Use integar-index to find element
-# print(emps_names.loc[9001:9002])  # Use integar-index to find element
+# print(emps_names.iloc[0])  # Use integer-index to find element
+# print(emps_names.loc[9001:9002])  # Use integer-index to find element
 #
 ##############
 # # Combining multiple Series into a dataframe
 ##############
-# data = ['jeff', 'jane', 'tom']
-# nums = ['234567', '7843285943', '98345947835']
-# emps_phones = pd.Series(nums, index=[9001, 9002, 9003], name='numbers')
-# emps_emails = pd.Series(data, index=[9001, 9002, 9003], name='emails')
-# emps_names.name = 'names'
-# df = pd.concat([emps_names, emps_emails, emps_phones], axis=1)
+data = ['jeff', 'jane', 'tom']
+nums = ['234567', '7843285943', '98345947835']
+emps_phones = pd.Series(nums, index=[9001, 9002, 9003], name='numbers')
+emps_emails = pd.Series(data, index=[9001, 9002, 9003], name='emails')
+emps_names.name = 'names'
+df = pd.concat([emps_names, emps_emails, emps_phones], axis=1)
 # print(df)
 
 ##############
 # Creating Pandas dataframe using API data = yfinance
 ##############
-# import yfinance as yf
-# tkr = yf.Ticker('TSLA')
-# hist = tkr.history(period='5d')
-# hist = hist.drop("Dividends", axis = 1)
-# hist = hist.drop('Stock Splits', axis = 1)
-# hist = hist.reset_index()
-# hist = hist.set_index('Date')
+import yfinance as yf
+tkr = yf.Ticker('TSLA')
+hist = tkr.history(period='5d')
+hist = hist.drop("Dividends", axis = 1)
+hist = hist.drop('Stock Splits', axis = 1)
+hist = hist.reset_index()
+hist = hist.set_index('Date')
 # print(hist)
 
 ##############
 # Converting JSON to Pandas dataframe
 #############
-# data = [
-#     {'Empno':9001, 'Salary': 3000},
-#     {'Empno':9002, 'Salary': 2800},
-#     {'Empno':9003, 'Salary': 2500},
-#     {'Empno':9005, 'Salary': 4000}
-# ]
-# json_data = json.dumps(data)
-# salary = pd.read_json(json_data)
-# salary = salary.set_index('Empno')
+data = [
+    {'Empno':9001, 'Salary': 3000},
+    {'Empno':9002, 'Salary': 2800},
+    {'Empno':9003, 'Salary': 2500},
+    {'Empno':9005, 'Salary': 4000}
+]
+json_data = json.dumps(data)
+salary = pd.read_json(json_data)
+salary = salary.set_index('Empno')
 # # print(salary)
 #
 # ##############
 # # Create dataframe from list of lists
 # ##############
-# data = [['9001', 'jeff', 'sales'],
-#         ['9002', 'jane', 'sales'],
-#         ['9003', 'tom', 'sales']]
-# emps = pd.DataFrame(data, columns=['Empno', 'Name', 'Job'])
-# column_types = {'Empno': int, 'Name':str, 'Job': str}
-# emps = emps.astype(column_types)
-# emps = emps.set_index('Empno')
+data = [['9001', 'jeff', 'sales'],
+        ['9002', 'jane', 'sales'],
+        ['9003', 'tom', 'sales']]
+emps = pd.DataFrame(data, columns=['Empno', 'Name', 'Job'])
+column_types = {'Empno': int, 'Name':str, 'Job': str}
+emps = emps.astype(column_types)
+emps = emps.set_index('Empno')
 # # print(emps)
 #
 # ##############
 # # Combining dataframes
 # ##############
 #
-# emps_salary = emps.join(salary)
+emps_salary = emps.join(salary)
 # # print(emps_salary)
-# new_emp = pd.Series({'Name': 'john', 'Job':'sales'}, name=9004) #  Adding new df which has rows with no matches to other df
-# emps = emps.append(new_emp)
+new_emp = pd.Series({'Name': 'john', 'Job':'sales'}, name=9004)  # Adding new df which has rows with no matches to other df
+emps = emps.append(new_emp)
 # # print(emps)
-# emps_salary = emps.join(salary)
+emps_salary = emps.join(salary)
 # # print(emps_salary)
 #
-# emps_salary = emps.join(salary, how='inner')  # 'inner' shows only the intersection of the combined df's
+emps_salary = emps.join(salary, how='inner')  # 'inner' shows only the intersection of the combined df's
 # # print(emps_salary)
 #
 # ##############
 # # One to many joins
 # ##############
 #
-# data=[[2608, 9001,35], [2617, 9001,35], [2620, 9001, 139], [2621, 9002,95], [2626, 9002,218]]
-# orders = pd.DataFrame(data, columns=['Pono', 'Empno', 'Total'])
+data=[[2608, 9001,35], [2617, 9001,35], [2620, 9001, 139], [2621, 9002,95], [2626, 9002,218]]
+orders = pd.DataFrame(data, columns=['Pono', 'Empno', 'Total'])
 # print(orders)
-# emps_orders = emps.merge(orders, how='inner', left_on='Empno', right_on='Empno').set_index('Pono')
+emps_orders = emps.merge(orders, how='inner', left_on='Empno', right_on='Empno').set_index('Pono')
 # print(emps_orders)
 #
 # ##############
@@ -131,24 +133,24 @@ import numpy as np
 ##############
 # Scikit-learn (used for machine learning)
 ##############
-# from sklearn.model_selection import train_test_split
-# from sklearn.feature_extraction.text import CountVectorizer
-# from sklearn.linear_model import LogisticRegression
-# df = pd.read_csv('amazon_cells_labelled.txt', names=['reviews', 'sentiment'], sep='\t')
-# reviews =df['reviews'].values
-# sentiments = df['sentiment'].values
-# reviews_train, reviews_test, sentiment_train, sentiment_test = train_test_split(reviews, sentiments, test_size=0.2, random_state=500)
-# vectoriser = CountVectorizer()
-# vectoriser.fit(reviews)
-# X_train = vectoriser.transform(reviews_train)
-# X_test = vectoriser.transform(reviews_test)
-# classifier = LogisticRegression()
-# classifier.fit(X_train, sentiment_train)
-# accuracy = classifier.score(X_test, sentiment_test)
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+df = pd.read_csv('amazon_cells_labelled.txt', names=['reviews', 'sentiment'], sep='\t')
+reviews = df['reviews'].values
+sentiments = df['sentiment'].values
+reviews_train, reviews_test, sentiment_train, sentiment_test = train_test_split(reviews, sentiments, test_size=0.2, random_state=500)
+vectoriser = CountVectorizer()
+vectoriser.fit(reviews)
+X_train = vectoriser.transform(reviews_train)
+X_test = vectoriser.transform(reviews_test)
+classifier = LogisticRegression()
+classifier.fit(X_train, sentiment_train)
+accuracy = classifier.score(X_test, sentiment_test)
 # print('Accuracy:', accuracy)
-#
-# new_reviews = ['Old version of python is useless', 'very good effort,  but not five stars', 'Clear and concise']
-# X_new = vectoriser.transform(new_reviews)
+
+new_reviews = ['Old version of python is useless', 'very good effort,  but not five stars', 'Clear and concise']
+X_new = vectoriser.transform(new_reviews)
 # print(classifier.predict(X_new))
 
 
